4865815cf8b34a2b7f05ce3506b02c1a6a2e9872
Benjamin <benjaminalladi10@gmail.com>
2024-02-19 11:33:48 +0530

    Number Recursion

---
+++ Recursion/rec1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Test{
+    static void ff(int c)
+    {
+        if(c==0)
+        {
+            return;
+        }
+        System.out.println("Hello! "+c);
+        ff(c-1);
+    }
+
+    static void fr(int c)
+    {
+        if(c==0)
+        {
+            return;
+        }
+        else
+        {
+            fr(c-1);
+            System.out.println("Hello! "+c);
+        }
+    }
+    public static void main(String[] args) {
+
+     Scanner sc= new Scanner(System.in);
+    //  ff(5);
+    //  fr(5);
+     sc.close();
+    }
+}
