1d83265bf435ff445b2d74556f2a11e15d6f8d42
Benjamin <benjaminalladi10@gmail.com>
2024-05-14 17:12:56 +0530

    Subsequence

--- Recursion/Subsequences/printSubsequence.java
+++ Recursion/Subsequences/printSubsequence.java
@@ -3,14 +3,26 @@ import java.util.*;
 
 class printSubsequence {
 
-    static void f(int ind,int[] a, ArrayList<Integer> al)
+    static void func(int ind,int[] a, ArrayList<Integer> al)
     {
+        if(ind==a.length)
+        {
+            System.out.println(al);
+            return;
+        }
 
+        // Take the element at index "i"
+        al.add(a[ind]);
+        func(ind+1,a,al);
+
+        // Not take the element at index "i"
+        al.remove(al.size()-1);
+        func(ind+1,a,al);
     }
     public static void main(String[] args) {
-        int[] a={3,1,2};
 
+        int[] a={3,1,2};
         ArrayList<Integer> al=new ArrayList<>();
-        f(0,a,al);
+        func(0,a,al);
     }
 }
---
+++ p.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class p {
+    public static void main(String[] args) {
+        ArrayList<Integer> al=new ArrayList<>();
+        al.add(2);
+        al.add(1);
+        al.add(2);
+
+        System.out.println(al);
+        al.remove(2);
+        System.out.println(al);
+    }
+}
